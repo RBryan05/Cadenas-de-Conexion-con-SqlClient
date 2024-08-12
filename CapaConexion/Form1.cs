2a4cdfce58db08de8a84c4c466ccc72a5adaa902
RBryan05 <rg0358032023@unab.edu.sv>
2024-08-12 17:36:58 -0600

    Agregar archivos de proyecto.

---
+++ CapaConexion/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CapaConexion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection
+                ("Data Source=DESKTOP-03PS3SB\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            conexion.Open();
+            MessageBox.Show("Conectado");
+            conexion.Close();
+            MessageBox.Show("Gracias, conexion finalizada");
+        }
+    }
+}
